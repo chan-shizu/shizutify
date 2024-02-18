38db516ef270c720e3a1554a3f25219dbb8d62b0
chan-shizu <bubbler018@gmail.com>
2024-02-18 12:40:06 +0900

    add: レクエスト送信処理のロジック

---
+++ app/api/request/route.ts
@@ -0,0 +1,32 @@
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { randomUUID } from "crypto";
+
+const dbClient = new DynamoDBClient({
+  credentials: {
+    accessKeyId: process.env.AWS_DYNAMO_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_DYNAMO_SECRET_ACCESS_KEY as string,
+  },
+  region: process.env.AWS_REGION,
+});
+const docClient = DynamoDBDocumentClient.from(dbClient);
+
+export async function POST(request: Request) {
+  try {
+    const { customerName, artistName, songName, other } = await request.json();
+    const command = new PutCommand({
+      TableName: process.env.AWS_DYNAMO_REQUEST_TABLE_NAME,
+      Item: {
+        request_id: randomUUID,
+        customer_name: customerName,
+        artist_name: artistName,
+        song_name: songName,
+        other: other,
+      },
+    });
+    const response = await docClient.send(command);
+    return Response.json({ status: 200 });
+  } catch {
+    return Response.json({ message: "post failed" }, { status: 400 });
+  }
+}
--- app/request/page.tsx
+++ app/request/page.tsx
@@ -14,6 +14,7 @@ import { SongsContext } from "@/provider/SongsProvider";
 import { error } from "console";
 import { useContext } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import toast, { Toaster } from "react-hot-toast";
 
 type Inputs = {
   customerName: string
@@ -39,33 +40,49 @@ export const Page = () => {
     handleSubmit,
     watch,
     formState: { errors },
+    reset
   } = useForm<Inputs>()
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<Inputs> = async(data) => {
+    const res = await fetch("api/request", {
+      method: "POST",
+      body: JSON.stringify(data)
+    })
+    if(res.ok) {
+      reset()
+      toast.success('リクエストを送信しました！曲が追加されるまで少々お待ちを！', {duration: 10000})
+    } else {
+      toast.error('エラーが発生しました、、直接連絡していただければ対応します、、', {duration: 10000})
+    }
+  }
 
   return (
     <div className={`bg-zinc-800 min-h-screen px-4 ${
       currentSongId !== "" ? "pb-48" : "pb-24"
     }`}>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
       <h1 className="text-center text-2xl pt-5 font-semibold">リクエスト</h1>
-      <p className="mt-4">希望の曲などがあればこちらからお願いします！</p>
+      <p className="mt-4 text-center">希望の曲などがあればこちらからお願いします！</p>
       <form className="mt-8 text-sm px-3" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col gap-y-2">
         <div>
           <label className="block">
-            あなたの名前(誰だか分かる名前だとうれしいです)
+            あなたの名前(非公開、出来たら誰だか分かる名前で！)
           </label>
-          <input className="rounded-lg mt-2 w-full h-8 text-black px-2" {...register("customerName", { required: "名前は必須です！"})} />
+          <input className="rounded-lg mt-2 w-full h-10 text-black px-2 text-lg" {...register("customerName", { required: "名前は必須です！"})} />
           <p className="h-6 text-red-400 mt-2">{errors.customerName?.message}</p>
         </div>
         <div>
           <label className="block">アーティスト名</label>
-          <input className="rounded-lg mt-2 w-full h-8 text-black px-2" {...register("artistName", { required: "アーティスト名は必須です！"})} />
+          <input className="rounded-lg mt-2 w-full h-10 text-black px-2 text-lg" {...register("artistName", { required: "アーティスト名は必須です！"})} />
           <p className="h-6 text-red-400 mt-2">{errors.artistName?.message}</p>
         </div>
         <div>
           <label className="block">曲名</label>
-          <input className="rounded-lg mt-2 w-full h-8 text-black px-2" {...register("songName", { required: "曲名は必須です！"})} />
+          <input className="rounded-lg mt-2 w-full h-10 text-black px-2 text-lg" {...register("songName", { required: "曲名は必須です！"})} />
           <p className="h-6 text-red-400 mt-2">{errors.songName?.message}</p>
         </div>
         <div>
@@ -73,7 +90,7 @@ export const Page = () => {
           <textarea rows={3} className="rounded-lg mt-2 w-full h-24 text-black px-2" {...register("other")} />
         </div>
         </div>
-        <input type="submit" className="mx-auto mt-8 py-3 w-full text-black bg-slate-400 rounded-full px-2" value="送信" />
+        <input type="submit" className="mx-auto mt-8 py-3 w-full bg-rose-500 rounded-full px-2 text-lg font-semibold" value="送信" />
       </form>
       {currentSongId !== "" && <AudioPlayerBottomBar />}
       <BottomBar />
