459e8f149918bc55552f1c5b30e5d0abba28366a
chan-shizu <bubbler018@gmail.com>
2024-02-20 22:11:47 +0900

    fix: requestが送信失敗するバグ修正

--- app/api/request/route.ts
+++ app/api/request/route.ts
@@ -14,14 +14,16 @@ const docClient = DynamoDBDocumentClient.from(dbClient);
 export async function POST(request: Request) {
   try {
     const { customerName, artistName, songName, other } = await request.json();
+    const currentDateTime = new Date();
     const command = new PutCommand({
       TableName: process.env.AWS_DYNAMO_REQUEST_TABLE_NAME,
       Item: {
-        request_id: randomUUID,
+        request_id: randomUUID(),
         customer_name: customerName,
         artist_name: artistName,
         song_name: songName,
         other: other,
+        created_at: currentDateTime.toString(),
       },
     });
     const response = await docClient.send(command);
